346d3a4c7ccb9e9059f7ca08d11d7ee87d84ec79
Tedy Gabriel Moisa <moisatedy@gmail.com>
2024-01-29 21:24:07 +0100

    Init project mydealership

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    instrumentationHook: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
