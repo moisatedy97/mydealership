3da0b3d5ae528f65bb4734a24d37f783480614d0
Tedy Gabriel Moisa <moisatedy@gmail.com>
2024-02-05 23:43:25 +0100

    Removed authentication control inside the homepage.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,15 @@
 import { getTranslations } from "next-intl/server";
 
-import { AuthError, Session } from "@supabase/supabase-js";
-import supabaseServer from "@/supabase/config";
 import Hero from "../components/hero";
 import Main from "../components/main";
 
 export default async function Index() {
   const t = await getTranslations("index");
 
-  const { data, error }: { data: { session: Session | null }; error: AuthError | null } =
-    await supabaseServer().auth.getSession();
-
-  if (error) {
-    throw error;
-  }
-
-  if (data.session) {
-    return (
-      <>
-        <Hero />
-        <Main />
-      </>
-    );
-  }
+  return (
+    <>
+      <Hero />
+      <Main />
+    </>
+  );
 }
