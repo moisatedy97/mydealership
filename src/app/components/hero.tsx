dfc63d0421a8f6dd9c0c55b1bac03590a40df00b
Kevin Agyeman <kevinagyemann@gmail.com>
2024-02-05 23:18:40 +0100

    homepage setup

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div
+        className="hero h-[75vh]"
+        style={{
+          backgroundImage:
+            "url(https://www.bmw.it/content/dam/bmw/common/all-models/m-series/m3-sedan/2023/highlights/bmw-3-series-cs-m-automobiles-sp-desktop.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Find Your best car</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+              deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
