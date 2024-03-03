778820e00d790188f3e980aa76c9d4f2dc16f8a9
Tedy Gabriel Moisa <moisatedy@gmail.com>
2024-03-03 16:34:49 +0100

    Fixed post request for stripe webhook

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,25 +1,27 @@
-import { buffer } from "stream/consumers";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 
-export async function POST(req: any) {
+export async function POST(req: Request) {
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-  try {
-    const signature = headers().get("stripe-signature");
-    const event: Stripe.Event = stripe.webhooks.constructEvent(await buffer(req.body), signature!, webhookSecret);
+  if (req.body) {
+    try {
+      const signature = headers().get("stripe-signature");
+      const bodyBuffer = await req.text();
+      const event: Stripe.Event = stripe.webhooks.constructEvent(bodyBuffer, signature!, webhookSecret);
 
-    switch (event.type) {
-      case "checkout.session.completed":
-        const checkoutSession = event.data.object;
-        console.log("Checkout session completed!", checkoutSession);
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
+      switch (event.type) {
+        case "checkout.session.completed":
+          const checkoutSession = event.data.object;
+          console.log("Checkout session completed!", checkoutSession);
+          break;
+        default:
+          return Response.json({ error: `Unhandled event type ${event.type}` }, { status: 400 });
+      }
+    } catch (error) {
+      return Response.json({ error: `Webhook Error: ${error}` }, { status: 400 });
     }
-  } catch (error: any) {
-    return Response.json({ error: `Webhook Error: ${error?.message}` }, { status: 400 });
   }
 
   return Response.json({ received: true });
