7caa562de6af94c014b9d3a6988fb57c94fbf2d4
Tedy Gabriel Moisa <moisatedy@gmail.com>
2024-04-21 18:58:45 +0200

    Fixed crontab

--- src/app/api/crontab/route.ts
+++ src/app/api/crontab/route.ts
@@ -1,19 +1,30 @@
+import { QueryData, QueryError } from "@supabase/supabase-js";
 import { mailTransporter } from "@/mailer";
+import supabaseServer from "@/supabase/config";
 
 export async function GET() {
   const isEmailActive: string | undefined = process.env.NEXT_EMAIL;
 
   if (isEmailActive && isEmailActive === "true") {
-    try {
-      await mailTransporter.sendMail({
-        to: process.env.NEXT_EMAIL_TO,
-        subject: "My Dealership website reset",
-        text: "Supabase request was sent. Supabase was restarted.",
-      });
+    const query = supabaseServer().from("Car").select("*");
+    const { data, error }: { data: QueryData<typeof query> | null; error: QueryError | null } = await query;
 
-      return new Response("Email sent", { status: 200 });
-    } catch (error) {
-      return new Response("Send email error", { status: 400 });
+    if (error) {
+      throw error;
+    }
+
+    if (data && data[0].carId > 0) {
+      try {
+        await mailTransporter.sendMail({
+          to: process.env.NEXT_EMAIL_TO,
+          subject: "My Dealership website reset",
+          text: "Supabase request was sent. Supabase was restarted.",
+        });
+
+        return new Response("Email sent", { status: 200 });
+      } catch (error) {
+        return new Response("Send email error", { status: 400 });
+      }
     }
   }
 
